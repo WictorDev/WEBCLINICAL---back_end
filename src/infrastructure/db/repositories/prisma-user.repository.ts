e21058460f76c207a22018e95240fecfd3aec41c
Witinhoapelão <wictorpedro95@gmail.com>
2025-04-09 11:41:10 -0300

    use case estruturado - user 1.0

---
+++ src/infrastructure/db/repositories/prisma-user.repository.ts
@@ -0,0 +1,114 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/domain/repositories/user.repository';
+import { PrismaService } from 'src/core/services/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { User } from 'src/domain/entities/user';
+import UniqueEntityCpf from 'src/core/entities/unique-entity-cpf';
+
+@Injectable()
+export class PrismaUserRepository implements UserRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(user: User): Promise<User> {
+    const hashedPassword = await bcrypt.hash(user.password, 4);
+  
+    const createdUser = await this.prismaService.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: hashedPassword,
+        cpf: user.cpf.toString(),
+        companyId: user.companyId,
+        typeId: user.typeId,
+        active: user.active ?? true,
+      }
+    })
+  
+    return new User({
+      ...createdUser,
+      cpf: new UniqueEntityCpf(createdUser.cpf),
+    });
+  }
+
+  async update(
+    cpf: string,
+    user: Partial<Omit<User, 'cpf'>>,
+  ): Promise<User> {
+  if (user.password) {
+    user.password = await bcrypt.hash(user.password, 4);
+  }
+  const updatedUser = await this.prismaService.user.update({
+    where: { cpf: cpf.toString() },
+    data: {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      companyId: user.companyId,
+      typeId: user.typeId,
+      active: user.active ?? true
+    }
+  })
+
+  return new User({
+    ...updatedUser,
+    cpf: new UniqueEntityCpf(updatedUser.cpf),
+  });
+}
+
+
+async findAll(): Promise<User[]> {
+  const users = await this.prismaService.user.findMany();
+
+  return users.map((user) => {
+    return new User({
+      cpf: new UniqueEntityCpf(user.cpf.toString()),
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      companyId: user.companyId,
+      typeId: user.typeId,
+      active: user.active,
+    });
+  });
+}
+async findByEmail(email: string): Promise<User | null> {
+  const user = await this.prismaService.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    return null;
+  }
+
+  return new User({
+    cpf: new UniqueEntityCpf(user.cpf.toString()),
+    name: user.name,
+    email: user.email,
+    password: user.password,
+    companyId: user.companyId,
+    typeId: user.typeId,
+    active: user.active,
+    
+  });
+}
+
+async findByCpf(cpf: string): Promise<User | null> {
+  const user = await this.prismaService.user.findUnique({
+    where: { cpf },
+  });
+
+  if (!user) return null;
+
+  return new User({
+    cpf: new UniqueEntityCpf(user.cpf.toString()),
+    name: user.name,
+    email: user.email,
+    password: user.password,
+    companyId: user.companyId,
+    typeId: user.typeId,
+    active: user.active,
+    
+  });
+
+
+}}
--- src/infrastructure/modules/auth.module.ts
+++ src/infrastructure/modules/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
-import { AuthController } from 'src/controllers/auth.controller';
+import { AuthController } from 'src/infrastructure/controllers/auth.controller';
 import { PrismaService } from '../services/prisma.service';
 
 @Module({
